9c0224e7f1f304c0b4c214744fa582f2f54170b2
Neophotokim <neophoto0924@gmail.com>
2025-09-24 17:42:21 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const img = document.getElementById("draggable");
+
+let offsetX, offsetY, isDragging = false;
+
+img.addEventListener("mousedown", (e) => {
+    isDragging = true;
+    offsetX = e.clientX - img.offsetLeft;
+    offsetY = e.clientY - img.offsetTop;
+    img.style.cursor = "grabbing";
+});
+
+document.addEventListener("mousemove", (e) => {
+    if (!isDragging) return;
+    img.style.left = `${e.clientX - offsetX}px`;
+    img.style.top = `${e.clientY - offsetY}px`;
+});
+
+document.addEventListener("mouseup", () => {
+    isDragging = false;
+    img.style.cursor = "grab";
+});
